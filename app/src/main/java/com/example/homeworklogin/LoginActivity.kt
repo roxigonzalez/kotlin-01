0d2346bd76edacc9c9d415124e1ba93973abd316
Roxy <roxigo@hotmail.com>
2023-03-10 14:52:59 -0600

    Login using intent to connect with other activity

---
+++ app/src/main/java/com/example/homeworklogin/LoginActivity.kt
@@ -0,0 +1,50 @@
+package com.example.homeworklogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.content.Intent
+import android.app.AlertDialog
+
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var mEmailText: EditText
+    private lateinit var mMessageText: EditText
+    private lateinit var mSendButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        bind()
+        listenEvent()
+
+    }
+    private fun bind() {
+        // code to get email and message from ID
+        mEmailText = findViewById(R.id.text_email)
+        mMessageText = findViewById(R.id.text_message)
+        mSendButton = findViewById(R.id.button_send)
+    }
+    private fun listenEvent() {
+        mSendButton.setOnClickListener {
+            // code to send data somewhere :) then start activity of message
+            val email = mEmailText.text.toString()
+            val message= mMessageText.text.toString()
+
+            if(email.trim().isNullOrBlank() || message.trim().isNullOrBlank()){
+                basicAlert()
+            } else {
+                val intent = Intent(this, MessageActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+    private fun basicAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Errors")
+        builder.setMessage("Please, fill the information")
+        builder.setPositiveButton("OK", null);
+        builder.show()
+    }
+}
---
+++ app/src/main/java/com/example/homeworklogin/MessageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.homeworklogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message)
+    }
+}
